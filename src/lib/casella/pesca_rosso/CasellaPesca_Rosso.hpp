06bef944f09906961052becac2b91c1b3e631d9e
Alexshev3 <alex.bortolotti.3@gmail.com>
2018-09-26 18:48:54 +0200

    NUOVA CASELLA PESCA_ROSSO

---
+++ src/lib/casella/pesca_rosso/CasellaPesca_Rosso.hpp
@@ -0,0 +1,19 @@
+/*
+ * CasellaPesca_Rosso.hpp
+ *
+ *  Created on: 26 set 2018
+ *      Author: alexb
+ */
+
+#ifndef LIB_CASELLA_PESCA_ROSSO_CASELLAPESCA_ROSSO_HPP_
+#define LIB_CASELLA_PESCA_ROSSO_CASELLAPESCA_ROSSO_HPP_
+
+#include "../Casella.hpp"
+
+class CasellaPesca_Rosso : public Casella {
+    public:
+        /// Costruttore
+        CasellaPesca_Rosso();
+};
+
+#endif /* LIB_CASELLA_PESCA_ROSSO_CASELLAPESCA_ROSSO_HPP_ */
